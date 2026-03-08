6c90de749c2e200abec753c674fa64d352cb851a
ahmedMahfouz40 <ahmedmhfouz440@gmail.com>
2026-03-08 21:56:46 +0200

    test for update after initial upload

--- src/Pages/Loading/Loading.jsx
+++ src/Pages/Loading/Loading.jsx
@@ -7,7 +7,7 @@ const Loading = () => {
     <div className="absolute z-50 inset-0 flex items-center justify-center bg-[rgba(0,0,0,0.7)]">
       <p className="bg-white rounded-3xl p-3    font-semibold">
         {" "}
-        <FontAwesomeIcon icon={faSpinner} spin className=" text-blue-700" />
+        <FontAwesomeIcon icon={faSpinner} spin className=" text-blue-600 me-1" />
         Refreshing Your Timeline...
       </p>
     </div>
