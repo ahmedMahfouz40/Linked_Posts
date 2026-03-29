9e19872fae8e959118910ead3c090d7dd696cdf7
ahmedMahfouz40 <ahmedmhfouz440@gmail.com>
2026-03-29 02:41:55 +0200

    new commit

--- src/CustomHooks/useGetPosts.js
+++ src/CustomHooks/useGetPosts.js
@@ -1,22 +1,3 @@
-// import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
-// import headerObject from "../TemplateLogic/headerObject";
-
-// function useGet(queryKey, endPoint, isEnabled) {
-//   const getPosts = () =>
-//     axios.get(`https://route-posts.routemisr.com/${endPoint}`, headerObject);
-//   const { data, isError, isFetched, isLoading, isFetching } = useQuery({
-//     queryKey: [...queryKey],
-//     queryFn: getPosts,
-//     enabled: isEnabled,
-//     refetchOnWindowFocus: false,
-//     staleTime: 1000 * 60 * 5,
-//     keepPreviousData: true,
-//   });
-//   return { data, isError, isFetched, isLoading, isFetching };
-// }
-
-// export default useGet;
 
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
@@ -26,7 +7,7 @@ function useGet(queryKey, endPoint, isEnabled, staleTime = " 1000 * 60 * 5") {
   const getPosts = () =>
     axios.get(
       `https://route-posts.routemisr.com/${endPoint}`,
-      getHeaderObject(), // ✅ called as a function, reads fresh token every time
+      getHeaderObject(), 
     );
 
   const { data, isError, isFetched, isLoading, isFetching } = useQuery({
